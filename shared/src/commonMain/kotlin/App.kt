0c50b884a83aa6b7090ff8aca19e58f14c28e4c3
Pallaw Pathak <pallaw.pathak@gmail.com>
2023-09-15 04:06:45 +0530

    Added filter buttons and used custom theme

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,5 +1,8 @@
+import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -8,10 +11,24 @@ import screens.BirdListScreen
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
-    MaterialTheme {
+    BirdAppTheme {
         val birdViewModel = getViewModel(Unit, viewModelFactory { BirdViewModel() })
         BirdListScreen(birdViewModel)
     }
 }
 
-expect fun getPlatformName(): String
+@Composable
+fun BirdAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = MaterialTheme.colors.copy(primary = Color.Black),
+        shapes = MaterialTheme.shapes.copy(
+            small = AbsoluteCutCornerShape(0.dp),
+            medium = AbsoluteCutCornerShape(0.dp),
+            large = AbsoluteCutCornerShape(0.dp),
+        )
+    ) {
+        content()
+    }
+}
--- shared/src/commonMain/kotlin/screens/BirdListScreen.kt
+++ shared/src/commonMain/kotlin/screens/BirdListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -30,15 +32,20 @@ fun BirdListScreen(
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 5.dp),
-            horizontalArrangement = Arrangement.spacedBy(5.dp)
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
         ) {
             for (category in uiState.categories) {
                 Button(
-                    onClick = {viewModel.updateCategory(category)}
+                    onClick = { viewModel.updateCategory(category) },
+                    modifier = Modifier.aspectRatio(1.0f).fillMaxSize().weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 0.dp,
+                        focusedElevation = 0.dp
+                    )
                 ) {
                     Text(category)
                 }
@@ -49,7 +56,9 @@ fun BirdListScreen(
                 columns = GridCells.Fixed(2),
                 horizontalArrangement = Arrangement.spacedBy(5.dp),
                 verticalArrangement = Arrangement.spacedBy(5.dp),
-                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 5.dp),
                 content = {
                     items(uiState.selectedImages) {
                         BirdImage(it)
